4187a1eaff9a9ea95cd7707300cd9397929d1012
Eugen Kobchuk <irravadi@gmail.com>
2023-04-15 18:51:22 +0300

    hm_1_done

--- Function_my.py
+++ Function_my.py
@@ -1,1 +1,25 @@
+#Напишіть функцію, яка приймає
+# список чисел та повертає їх середнє
+#  арифметичне значення.
+def string():
+    list = [1,2,3,4,5,6,7,8,9,10]
+    print(list)
+    print(sum(list)/len(list))
+string()
 
+def average(list):
+    return sum(list)/len(list)
+print(average([1,2,3,4,5,6,7,8,9,10]))
+
+def arithmetic_mean(numbers):
+  
+    if not numbers:
+        return 0
+    else:
+        return sum(numbers) / len(numbers)
+
+print(arithmetic_mean([1,2,3,4,5,6,7,8,9,10]))
+
+def mean(numbers):
+    return sum(numbers) / len(numbers)
+mean([1,2,3,4,5,6,7,8,9,10])
