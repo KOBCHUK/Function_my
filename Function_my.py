239db1e4eefcf2d7c44bb2c1b0aefda9aff4641d
Eugen Kobchuk <irravadi@gmail.com>
2023-04-17 16:21:10 +0300

    hm_7_done

--- Function_my.py
+++ Function_my.py
@@ -1,1 +1,12 @@
 
+#Створіть функцію, яка приймає
+#ціле число та повертає список
+#всіх дільників цього числа
+def div_number(num):
+    numbers = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            numbers.append(i)
+    return numbers
+print(div_number(10))
+print(div_number(234))
