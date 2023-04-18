4fcd318354a78390740eb67cb4b989969c390f8c
Eugen Kobchuk <irravadi@gmail.com>
2023-04-18 09:50:22 +0300

    done

--- Function_my.py
+++ Function_my.py
@@ -1,6 +1,3 @@
-#Напишіть функцію, яка приймає рядок
-# та повертає рядок з першою літерою кожного слова,
-#написаним великими літерами
 def my_func(string):
     words = string.split()
     capitalized_words = [word.capitalize() for word in words]
