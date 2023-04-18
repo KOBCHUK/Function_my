df4951b5e734c9bda29521cf6550927d126b93af
Eugen Kobchuk <irravadi@gmail.com>
2023-04-18 09:47:03 +0300

    done

--- Function_my.py
+++ Function_my.py
@@ -16,7 +16,3 @@ def arithmetic_mean(numbers):
         return sum(numbers) / len(numbers)
 
 print(arithmetic_mean([1,2,3,4,5,6,7,8,9,10]))
-
-def mean(numbers):
-    return sum(numbers) / len(numbers)
-mean([1,2,3,4,5,6,7,8,9,10])
